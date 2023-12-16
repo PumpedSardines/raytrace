63c06387fc1a691fe0c900a1f43df0bc851d003d
Fritiof Rusck <fritiof@rusck.se>
2023-12-16 13:34:18 +0100

    fix: lower samples on teapot

--- examples/teapot/main.rs
+++ examples/teapot/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 1920;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 10;
 
     let camera = Camera {
         origin: Point3::new(0.0, 5.0, 8.0),
